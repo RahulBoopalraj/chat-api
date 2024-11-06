311fd12347416c1bd774252fe30f414a74cfb7b2
sabari245 <sabari2004@hotmail.com>
2024-11-06 09:19:38 +0530

    updated the qna api;

--- routes/qna.ts
+++ routes/qna.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import db from "../utils/db";
+import { QnaFlowStatus } from "@prisma/client";
 
 const router = Router();
 
@@ -13,7 +14,7 @@ router.get("/get-flow", async (req, res) => {
       res.status(200).json({ data: flow });
     }
   } catch (error) {
-    console.error(error);
+    console.error("error getting the flow", error);
     res
       .status(500)
       .json({ message: "[Internal server error] could not get the flow" });
@@ -44,19 +45,113 @@ router.post("/set-flow", async (req, res) => {
       });
     }
   } catch (error) {
-    console.error(error);
+    console.error("error updating the flow", error);
     res
       .status(500)
       .json({ message: "[Internal server error] could not update the flow" });
   }
 });
 
-// for users to get the chat flow
-
 // for users to upload the full conversation with the client
+router.post("/upload-conversation", async (req, res) => {
+  const { conversation, userId } = req.body;
+
+  try {
+    await db.qnaFlowResponse.create({
+      data: {
+        userid: userId,
+        response: conversation,
+        status: QnaFlowStatus.NEW,
+      },
+    });
+  } catch (error) {
+    console.error("error uploading the conversation", error);
+    res.status(500).json({
+      message: "[Internal server error] could not upload the conversation",
+    });
+  }
+});
 
 // for admins to get the conversations
+router.get("/get-conversations", async (req, res) => {
+  const { status, skip, limit } = req.query;
+
+  // checking if status is in the type of qnaFlowStatus
+  if (
+    status &&
+    !Object.values(QnaFlowStatus).includes(status as QnaFlowStatus)
+  ) {
+    res.status(400).json({
+      message:
+        "Invalid status value: status has to be one of the predefined values",
+    });
+    return;
+  }
+
+  // checking if skip and limit are numbers
+  if (skip && isNaN(parseInt(skip as string))) {
+    res
+      .status(400)
+      .json({ message: "Invalid skip value: skip has to be numeric" });
+    return;
+  }
+
+  if (limit && isNaN(parseInt(limit as string))) {
+    res
+      .status(400)
+      .json({ message: "Invalid limit value: limit has to be numeric" });
+    return;
+  }
+
+  try {
+    const conversations = await db.qnaFlowResponse.findMany({
+      where: {
+        status: status ? (status as QnaFlowStatus) : undefined,
+      },
+      skip: skip ? parseInt(skip as string) : undefined,
+      take: limit ? parseInt(limit as string) : undefined,
+    });
+
+    res.status(200).json({ data: conversations });
+  } catch (error) {
+    console.error("error getting the conversation", error);
+    res.status(500).json({
+      message: "[Internal server error] could not get the conversation",
+    });
+  }
+});
 
 // for admins to update the status and notes assigned to a conversation
+router.post("/update-conversation", async (req, res) => {
+  const { conversationId, status, notes } = req.body;
+
+  // checking if status is in the type of qnaFlowStatus
+  if (
+    status &&
+    !Object.values(QnaFlowStatus).includes(status as QnaFlowStatus)
+  ) {
+    res.status(400).json({
+      message:
+        "Invalid status value: status has to be one of the predefined values",
+    });
+    return;
+  }
+
+  try {
+    await db.qnaFlowResponse.update({
+      where: {
+        id: conversationId,
+      },
+      data: status ? { status, notes } : { notes },
+    });
+
+    res.status(200).json({ message: "Conversation updated successfully" });
+  } catch (error) {
+    console.error("error updating the conversation", error);
+    res.status(500).json({
+      message: "[Internal server error] could not update the conversation",
+    });
+  }
+});
 
 export default router;
